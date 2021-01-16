9ddbac8c590a0bf137305a2a223d069e8751a68c
Bjornarovrebo <71792583+Bjornarovrebo@users.noreply.github.com>
2021-01-16 23:28:06 +0100

    Add files via upload

---
+++ level-2.js
@@ -0,0 +1,25 @@
+//level2Questions
+
+//question1 
+
+for (var i = 15; i <= 25; i++) {
+    if(i % 2 === 0) {
+    console.log(i);
+    }
+}
+
+//question2
+
+function multipleFunctions(innerFunction) {
+    var innerFunction = "What to do?";
+    console.log("I am a function");
+}
+
+function outerFunction(what) {
+    multipleFunctions("What?");
+}
+
+// I didn´t quite understand how to do this. 
+
+
+
